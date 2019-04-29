74f20e43b59aca5ba7eff69b6bc10fb2091f4ef7
github username <github email address>
2019-04-29 16:10:42 -0700

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    matched_array = []
+    array.each { |element|
+      if element.split(//).sort == @word.split(//).sort
+        matched_array << element
+      end
+    }
+    return matched_array
+  end
+end
